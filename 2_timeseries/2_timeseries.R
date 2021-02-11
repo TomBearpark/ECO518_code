b6384418aecc4ebd5ed8c3a63f9358a9485e73c3
TomBearpark <t.bearpark@googlemail.com>
2021-02-11 17:09:57 +0000

    clean up

--- 2_timeseries/2_timeseries.R
+++ 2_timeseries/2_timeseries.R
@@ -1,4 +1,6 @@
-# Code for pset 2
+# Code for PSET 2
+# Creates visualizations of the time series and ACFs for each of the 
+# models.
 
 ############################################
 # 0 Set up 
@@ -10,13 +12,26 @@ out <- paste0("/Users/tombearpark/Documents/princeton/1st_year/term2/",
               "ECO518_Metrics2/sims/exercises/2_AR_ts/")
 set.seed(1)
 
+# Function for saving a time series and acf model 
+create_plots <- function(df, problem){
+  ggplot(df) + 
+    geom_line(aes(x = t, y = y)) + 
+    geom_hline(yintercept = mean(df$y, na.rm = TRUE), 
+             color = "red", alpha = 0.3)
+  ggsave(paste0(out, "p", problem , "_ts.png"), height = 4, width = 5)
+
+  png(paste0(out, "p", problem, "_acf.png"))
+    acf(df$y, main = "ACF")
+  dev.off()
+}
+
 ############################################
 # Problem 1
 ############################################
 
 # Generate data
 N <- 100
-df <- 
+df1 <- 
   tibble(u = runif(N, 0, 1)
                  ) %>% 
   mutate(t = row_number(), 
@@ -29,56 +44,32 @@ df <-
          ) %>% 
   dplyr::select(-u) 
 # Plot
-ggplot(df) + 
-  geom_line(aes(x = t, y = y)) + 
-  geom_hline(yintercept = mean(df$y, na.rm = TRUE), 
-             color = "red", alpha = 0.3)
-ggsave(paste0(out, "p1_ts.png"), height = 4, width = 5)
-
-png(paste0(out, "p1_acf.png"))
-  acf(df$y, main = "ACF")
-dev.off()
+create_plots(df1, 1)
 
 ############################################
 # Problem 2
 ############################################
 
 N <- 100
-df <- 
+df2 <- 
   tibble(epsilon = rnorm(N, mean = 0, sd = 1)) %>% 
   mutate(t = row_number(), 
          y = epsilon + 2 * lag(epsilon) + 0.64 * lag(epsilon, 2))
-
-ggplot(df) + 
-  geom_line(aes(x = t, y = y)) + 
-  geom_hline(yintercept = mean(df$y, na.rm = TRUE), 
-             color = "red", alpha = 0.3)
-ggsave(paste0(out, "p2_ts.png"), height = 4, width = 5)
-
-png(paste0(out, "p2_acf.png"))
-  acf(df$y[!is.na(df$y)], main = "ACF")
-dev.off()
+# Plot
+create_plots(df2, 2)
 
 ############################################
 # Problem 3
 ############################################
 
 N <- 100
-df <- 
+df3 <- 
   tibble(epsilon = rnorm(N, mean = 0, sd = 1)) %>% 
   mutate(t = row_number(), 
          y = epsilon + 1.5 * lag(epsilon) + 0.5 * lag(epsilon, 2))
 
-ggplot(df) + 
-  geom_line(aes(x = t, y = y)) + 
-  geom_hline(yintercept = mean(df$y, na.rm = TRUE), 
-             color = "red", alpha = 0.3)
-ggsave(paste0(out, "p3_ts.png"), height = 4, width = 5)
-
-png(paste0(out, "p3_acf.png"))
-  acf(df$y[!is.na(df$y)], main = "ACF")
-dev.off()
-
+# Plot
+create_plots(df3, 3)
 
 
 
