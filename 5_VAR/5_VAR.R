aad340e0ebdf1993e7ddf91d91f2b7f7705ffc6b
TomBearpark <t.bearpark@googlemail.com>
2021-03-08 11:40:29 +0000

    final version

--- 5_VAR/5_VAR.R
+++ 5_VAR/5_VAR.R
@@ -16,19 +16,21 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(ggfortify)
-# library(patchwork)
 library(stargazer)
 library(xtable)
 theme_set(theme_bw())
 
-
-source("vecmlh.R")
 load("pdat.RData")
 df <- pdat
 
 ####################################################################
 # 1. Plot the data 
 ####################################################################
+
+# AIC suggests 5 lags, btw
+pacman::p_load(vars)
+var_aic <- VAR(df, type = "const", lag.max = 10, ic = "AIC", season = 4)
+var_aic$p
 
 p1 <- autoplot(df, scales = "fixed")
 ggsave(p1, file = paste0(out, "1_ts_faceted.png"), height = 6, width = 9)
@@ -41,10 +43,9 @@ ggsave(p2, file = paste0(out, "1_ts.png"), height = 5, width = 7)
 ####################################################################
 # Estimate a 3 lag VAR
 ?mgnldnsty
-# Note, had to give a vprior argument to get this to work. Not sure if
-# what I have done here is correct. need to discuss
+# Note, had to give a vprior argument to get this to work. 
 
-VAR_and_forecast <- function(lags, df){
+VAR_and_forecast <- function(lags, df, w_prior = 0){
   
   if(lags == 3) df <- df[-(1:6), ]
   if(! lags %in% c(3,9)) stop("Not implemented for those lags")
@@ -54,7 +55,7 @@ VAR_and_forecast <- function(lags, df){
                     lags = lags,
                     xdata = NULL,
                     const = TRUE, 
-                    vprior = list(sig = 1, w = 0))
+                    vprior = list(sig = 1, w = w_prior))
 
   # Format initial condition
   y0 <- tail(df, lags)
@@ -80,6 +81,11 @@ VAR_and_forecast <- function(lags, df){
 VAR3 <- VAR_and_forecast(3, df)
 VAR9 <- VAR_and_forecast(9, df)
 
+# Compare to version with different prior
+VAR3_ERIC <- VAR_and_forecast(3, df, w_prior = 20)
+
+VAR3_ERIC$var$var$By[,,1] == VAR3$var$var$By[,,1]
+
 VAR3$var$prior 
 VAR3$var$var$Bx 
 
@@ -96,9 +102,6 @@ for(i in 1:3)
 for(i in 1:9)
   convert_to_latex_matrix(VAR9$var$var$By[,,i] )
 
-
-VAR9$var$prior
-
 # 2.1. Plot the forecasts
 plot_df <- df %>% 
   as_tibble() %>% 
@@ -113,11 +116,10 @@ plot_df %>% filter(n>-100) %>%
   facet_wrap(~name, scales = "fixed")
 ggsave(paste0(out, "2_var_forecast_plots.png"), height = 5, width = 8)  
 
-# 2.2. Compare the log odds ratio:
+# 2.2. Compare the log odds ratio, print a table for latex
 data.frame(VAR = c(3, 9, "Odds Ratio"), 
            Value = c(VAR3$var$w , VAR9$var$w, VAR3$var$w / VAR9$var$w)) %>% 
   stargazer(summary = FALSE)
-
 
 ####################################################################
 # 3. Check the roots
@@ -137,7 +139,6 @@ mat9 <- sysmat(By=VAR9$var$var$By)
 mat9_eigen <- eigen(mat9)
 mat9_eigen$values[abs(mat9_eigen$values - 1) < 1/T]
 
-
 ####################################################################
 # 4. Plot the IR Plots
 ####################################################################
@@ -147,20 +148,32 @@ plotir(resp3, file = paste0(out, "4_VAR3_IRF.pdf"), main = "IR Plot VAR 3")
 resp9 <- impulsdtrf(VAR9$var$var, nstep = 48)
 plotir(resp9, file = paste0(out, "4_VAR9_IRF.pdf"), main = "IR Plot VAR 9")
 
-
 ####################################################################
 # 5. VECM
 ####################################################################
-VECM <- vecmlh(
-  beta = VAR3$var$var$By, 
-  y = df, 
-  lags = 3,
-  ncoint = 1, verbose = TRUE)
+pacman::p_load(tsDyn)
+est_VECM <- VECM(df, lag = 3, 
+                  r = 1, include = "none", estim = "2OLS")
+est_VECM %>% summary()
+toLatex((est_VECM), parenthese="Pvalue")
+coefB(est_VECM)
+
+dfV <- 
+  predict(est_VECM, n.ahead = 100) %>% 
+  data.frame() %>% 
+  mutate(model = "VECM", n= row_number(), lags= "VECM") %>% 
+  pivot_longer(cols = c("raw", "intermediate", "final")) %>% 
+  bind_rows(plot_df %>% filter(lags %in% c("historic", "3"), n > 0) ) %>% 
+  mutate(model = ifelse(is.na(model), "VAR3", model))
+
+ggplot(dfV)+
+  geom_line(aes(x = n, y = value, color = name)) +
+  facet_wrap(~model)
+ggsave(paste0(out, "5_vecm.png"))
 
 ####################################################################
 # 6. State space
 ####################################################################
-
 
 
 
