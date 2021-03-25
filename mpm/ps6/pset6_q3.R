07a2802d01ed2190251b9a2da85d9c5c29bee511
TomBearpark <t.bearpark@googlemail.com>
2021-03-25 14:32:27 +0000

    absolutevalue bug

--- mpm/ps6/pset6_q3.R
+++ mpm/ps6/pset6_q3.R
@@ -2,7 +2,7 @@
 
 library(tidyverse)
 library(sandwich)
-library(furrr) #parallel functional programming 
+library(furrr) # parallel functional programming 
 
 ###########################################################
 # Functions 
@@ -35,7 +35,7 @@ resid_boot <- function(df, i, formula, lm1, beta_hat){
 }
 
 MC_draw <- function(i, alpha_0, beta_0, sigma_X_0, B = 500){
-  message(i)
+  
   # simulate the data
   N       <- 50
   
@@ -51,47 +51,50 @@ MC_draw <- function(i, alpha_0, beta_0, sigma_X_0, B = 500){
   # Calculate t-stat
   fit      <- lm(data = df, Y ~ X)
   beta_hat <- coef(fit)["X"]
-  sd_hat   <- sqrt(vcovHC(fit, type="HC1")[2,2]) 
+  sd_hat   <- sqrt(vcovHC(fit, type="HC1")["X","X"]) 
   
-  t        <- coef(fit)["X"] / sd_hat
-  t_tilde <- sqrt(N) * beta_hat
-  
-  QQ <- c(0.025, 0.975)
+  t        <- abs(coef(fit)["X"] / sd_hat)
+  t_tilde  <- abs(sqrt(N) * beta_hat)
   
   # (a) Asymtotically normal critical value:
-  Ca <- qnorm(QQ, mean = 0, sd = 1)
-  a  <- (1- 1*between(t, Ca[1], Ca[2]))
-  
-  print("done A")
-  print(Ca)
+  Ca <- abs(qnorm((1-.95)/2))
+  a  <- as.numeric((t > Ca))
   
   # (b) nonparametric bootstrap critical value
   draws_npm <- map_dfr(seq(1:B), npm_boot, df = df, 
                        formula = "Y ~ X", beta_hat = beta_hat)
-  Cb <-  quantile(draws_npm$T, QQ) 
-  b  <- (1- 1*between(t, Cb[1], Cb[2]))
-  print("done B")
-  print(Cb)
+  
+  Cb <- quantile(abs(draws_npm$T), .95)
+  b  <- as.numeric((t > Cb))
   
   # (c) residual bootstrap critical value.
   draws_resid <- map_dfr(seq(1:B), resid_boot, df = df, lm1 = fit, 
                        formula = "Y ~ X", beta_hat = beta_hat)
-  Cc <- quantile(draws_resid$T, QQ)
-  c  <- (1- 1*between(t, Cc[1], Cc[2]))
-  print("done C")
-  print(Cc)
+  Cc <- quantile(abs(draws_resid$T), .95)
+  c  <- as.numeric((t > Cc))
   
-  tibble(draw = i, t = t, a = a, b = b, c = c)
+  
+  # (d)
+  Cd <- quantile(abs(sqrt(N) * (draws_npm$beta - beta_hat)), .95)
+  d  <- as.numeric((t_tilde > Cd))
+  
+  # (e)
+  Ce <- quantile(abs(sqrt(N) * (draws_resid$beta - beta_hat)), .95)
+  e  <- as.numeric((t_tilde > Ce))
+  
+  tibble(draw = i, t = t, t_tilde = t_tilde, 
+         a = a, b = b, c = c, d = d, e = e)
 }
 
 ###########################################################
-# Functions 
+# Run code
+
 
 alpha_0 <- 1
 beta_0 <- 0
 sigma_X_0 <- function(X) 1
 
-MC_draw(i = 1, alpha_0, beta_0, sigma_X_0, B = 10)
+MC_draw(i = 1, alpha_0, beta_0, sigma_X_0, B = 1000)
 
 # Parallel compute: 8 workers 
 plan(multisession, workers = 8)
@@ -107,6 +110,7 @@ sum_i <- summarise_all(res_i, mean) %>%
 
 # Part (ii)
 sigma_X_0_ii <- function(X) 0.5 * X
+
 res_ii <- future_map_dfr(seq(1,1000), MC_draw, 
                       alpha_0 = alpha_0, beta_0 = beta_0, 
                       sigma_X_0 = sigma_X_0_ii, B = 500, 
@@ -115,6 +119,6 @@ res_ii <- future_map_dfr(seq(1,1000), MC_draw,
 sum_ii <- summarise_all(res_ii, mean) %>% 
   mutate(part = "ii") %>% select(-c(draw, t)) %>% data.frame()
 
-
+bind_rows(sum_i, sum_ii)
 
 
