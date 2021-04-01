56c5d12bddc4e45d6f008410044a4913142fbed8
TomBearpark <t.bearpark@googlemail.com>
2021-04-01 13:49:58 +0100

    clean up

--- mpm/ps7/pset7.R
+++ mpm/ps7/pset7.R
@@ -1,17 +1,15 @@
 # code for MPM pset 7
 
 rm(list = ls())
-
 library(tidyverse) 
 library(furrr)
-
 theme_set(theme_bw())
 dir <- paste0("/Users/tombearpark/Documents/princeton/1st_year/term2/", 
               "ECO518_Metrics2/mpm/excercises/ps7/")
 out <- paste0(dir, "out/")
 setwd(dir)
 set.seed(1)
-
+plan(multisession, workers = 8)
 ###########################################################
 # Question 1
 ###########################################################
@@ -81,9 +79,39 @@ ggplot(df) +
 
 
 # Returns weight for observation i at X0
-w_i <- function(Xi, X0, X, h, type = "kernel"){
-  if(type == "kernel") K(Xi, X0, h) / sum(K(X, X0, h))
+w_i <- function(Xi, X0, X, h, type){
+  
+  if(type == "kernel") {
+    
+    w_val <- K(Xi, X0, h) / sum(K(X, X0, h))
+    
+  }else if (type == "local_linear"){
+    
+    z  <- matrix(c(1, X0))
+    Zi <- matrix(c(1, Xi))
+    mid <- matrix(c(0,0,0,0), nrow = 2)
+    for(j in 1:length(X)){
+      Zj <- matrix(c(1, X[j]))
+      mid <- mid + K(X[j], X0, h) * Zj %*% t(Zj)
+    }
+    w_val <- (t(z) %*% solve(mid) %*% (K(Xi, X0, h) * Zi)) %>% as.numeric()
+    
+  }else if (type == "poly"){
+    
+    p <- h
+    Zi <- z(X0, p)
+    zx <- z(Xi, p)
+    mid <- matrix(0, p+1, p+1)
+    for(j in 1:length(X)){
+      Zj <- z(X[j], p)
+      mid <- mid +  Zj %*% t(Zj)
+    }
+    w_val <-t(zx) %*% solve(mid) %*% Zi %>% as.numeric()
+  }else stop("not implemented")
+  
+  return(w_val)
 }
+
 
 # Get predicted value at X0
 m <- function(X0, X, h, Y, type = "kernel"){
@@ -123,14 +151,16 @@ CV_star <- CV_results$h[CV_results$CV == min(CV_results$CV, na.rm = T)]
 # Plot!
 ggplot(CV_results) + 
   geom_line(aes(x = h, y= CV)) + 
-  ggtitle(paste0("h* = ", CV_star))
+  ggtitle(paste0("h* = ", CV_star))+ 
+  ggsave(paste0(out, "/kernel_CV.png"))
 
 range <- seq(min(X), max(X), length.out = 100)
 map_dfr(range, m, X = X, h = CV_star, Y = Y) %>% 
   ggplot() + 
   geom_line(aes(x = x, y = m)) + 
   geom_point(data = df, aes(x = log_inc, y = share_food), alpha = 0.2) +
-  ylim(c(0,1)) + xlim(5.5, 9)
+  ylim(c(0,1)) + xlim(5.5, 9)+ 
+  ggsave(paste0(out, "/kernel_w_scatter.png"))
 
 ###########################################################
 # iii) Estimate the regression of share of food expenditures on log income 
@@ -140,50 +170,24 @@ map_dfr(range, m, X = X, h = CV_star, Y = Y) %>%
 # (a) For Local Linear Regression, calculate and plot the cross-validation 
 # criterion as a function of h (the bandwidth) on the interval (1, 2] at
 # 0.01 increments (e.g. h = 1.01, 1.02, . . . , 1.99, 2).
-
-w_i <- function(Xi, X0, X, h, type){
-  
-  if(type == "kernel") {
-    
-    w_val <- K(Xi, X0, h) / sum(K(X, X0, h))
-    
-  }else if (type == "local_linear"){
-    
-    z  <- matrix(c(1, X0))
-    Zi <- matrix(c(1, Xi))
-    
-    mid <- matrix(c(0,0,0,0), nrow = 2)
-    
-    for(j in 1:length(X)){
-      Zj <- matrix(c(1, X[j]))
-      mid <- mid + K(X[j], X0, h) * Zj %*% t(Zj)
-    }
-    w_val <- (t(z) %*% solve(mid) %*% (K(Xi, X0, h) * Zi)) %>% as.numeric()
-  
-  }else stop("not implemented")
-  
-  return(w_val)
-}
-
 # (b) Approximate the optimal bandwidth hC V using a grid search over the 
 # interval (1, 2] in 0.01 increments.
-plan(multisession, workers = 8)
+# (c) Use the optimal bandwidth to estimate the Local Linear Regression of 
+# share of food expenditures on log income at each value of X, 
+# where X is the log income data. Plot the estimated regression curve 
+# and a scatterplot of the data in the same graph.
 
+# This computation is very long!! 
 CV_results_ll <- 
   data.frame(h = hvals, 
              CV = future_map_dbl(hvals, CV, X = X, Y = Y, type = "local_linear"))
 
 h_star_ll <- CV_results_ll$h[CV_results_ll$CV == min(CV_results_ll$CV, na.rm = T)]
-
 write_csv(CV_results_ll, paste0(out, "local_linear_CV.csv"))
+
 ggplot(CV_results_ll) + 
   geom_line(aes(x = h, y = CV)) + 
   ggsave(paste0(out, "/local_linear_CV.png"))
-
-# (c) Use the optimal bandwidth to estimate the Local Linear Regression of 
-# share of food expenditures on log income at each value of X, 
-# where X is the log income data. Plot the estimated regression curve 
-# and a scatterplot of the data in the same graph.
 
 map_dfr(range, m, X = X, h = h_star_ll, Y = Y, type = "local_linear") %>% 
   ggplot() + 
@@ -192,8 +196,6 @@ map_dfr(range, m, X = X, h = h_star_ll, Y = Y, type = "local_linear") %>%
   geom_point(data = df, aes(x = log_inc, y = share_food), alpha = 0.2) +
   ylim(c(0,1)) + xlim(5.5, 9) + 
   ggsave(paste0(out, "/local_linear_w_scatter.png"))
-
-
 
 ###########################################################
 # iv) Estimate the regression of share of food expenditures on log income 
@@ -213,52 +215,12 @@ z <- function(x, p){
   matrix(z)
 }
 
-w_i <- function(Xi, X0, X, h, type){
-  
-  if(type == "kernel") {
-    
-    w_val <- K(Xi, X0, h) / sum(K(X, X0, h))
-    
-  }else if (type == "local_linear"){
-    
-    z  <- matrix(c(1, X0))
-    Zi <- matrix(c(1, Xi))
-    
-    mid <- matrix(c(0,0,0,0), nrow = 2)
-    
-    for(j in 1:length(X)){
-      Zj <- matrix(c(1, X[j]))
-      mid <- mid + K(X[j], X0, h) * Zj %*% t(Zj)
-    }
-    w_val <- (t(z) %*% solve(mid) %*% (K(Xi, X0, h) * Zi)) %>% as.numeric()
-    
-  }else if (type == "poly"){
-    p <- h
-    Zi <- z(X0, p)
-    zx <- z(Xi, p)
-    
-    mid <- matrix(0, p+1, p+1)
-    
-    for(j in 1:length(X)){
-      Zj <- z(X[j], p)
-      mid <- mid +  Zj %*% t(Zj)
-    }
-    
-    w_val <-t(zx) %*% solve(mid) %*% Zi %>% as.numeric()
-  
-  }else stop("not implemented")
-  
-  return(w_val)
-}
-
 pvals <- seq(1,10)
 CV_results_poly <- data.frame(h = pvals, CV = 0)
 
 for(p in pvals){
   print(p)
-  try(
-    CV_results_poly$CV[p] <- CV(p, X, Y, type = "poly")
-  )
+  try(CV_results_poly$CV[p] <- CV(p, X, Y, type = "poly"))
 }
 
 write_csv(CV_results_poly, paste0(out, "poly_CV.csv"))
@@ -268,8 +230,7 @@ ggplot(CV_results_poly) +
 
 # All the polys on one plot
 get_df_poly <- function(h) {
-  map_dfr(range, m, X = X, h = h, Y = Y, type = "poly") %>% 
-    mutate(h = h)
+  map_dfr(range, m, X = X, h = h, Y = Y, type = "poly") %>% mutate(h = h)
 }
 plot_df_poly <- map_dfr(seq(1,4), get_df_poly)
 
