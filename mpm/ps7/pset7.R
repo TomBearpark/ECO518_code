da94d219d08c3523d38481e68831b115912afee7
TomBearpark <t.bearpark@googlemail.com>
2021-04-01 13:34:16 +0100

    done the poly part

--- mpm/ps7/pset7.R
+++ mpm/ps7/pset7.R
@@ -132,7 +132,7 @@ map_dfr(range, m, X = X, h = CV_star, Y = Y) %>%
   geom_point(data = df, aes(x = log_inc, y = share_food), alpha = 0.2) +
   ylim(c(0,1)) + xlim(5.5, 9)
 
-
+###########################################################
 # iii) Estimate the regression of share of food expenditures on log income 
 #   (i.e. the Engel curve) using Local Linear Regression with 
 #   a normal kernel in three steps:
@@ -195,9 +195,93 @@ map_dfr(range, m, X = X, h = h_star_ll, Y = Y, type = "local_linear") %>%
 
 
 
+###########################################################
+# iv) Estimate the regression of share of food expenditures on log income 
+# (i.e. the Engel curve) using Polynomial Series Regression in three steps:
+# (a) For Polynomial Series Regression, calculate and plot the cross-validation 
+# criterion as a function of p (the order of the polynomial) 
+# on the grid {1, 2, . . . , 10}.
+# (b) Select the polynomial order that minimizes the cross-validation criterion.
+# (c) Use the optimal polynomial order to estimate the Polynomial Series 
+# Regression of share of food expenditures on log income at each 
+# value of X, where X is the log income data. Plot the estimated regression 
+# curve and a scatterplot of the data in the same graph.
 
+z <- function(x, p){
+  z <- c(1)
+  for(j in 1:p) z[j+1] <- x^j
+  matrix(z)
+}
 
+w_i <- function(Xi, X0, X, h, type){
+  
+  if(type == "kernel") {
+    
+    w_val <- K(Xi, X0, h) / sum(K(X, X0, h))
+    
+  }else if (type == "local_linear"){
+    
+    z  <- matrix(c(1, X0))
+    Zi <- matrix(c(1, Xi))
+    
+    mid <- matrix(c(0,0,0,0), nrow = 2)
+    
+    for(j in 1:length(X)){
+      Zj <- matrix(c(1, X[j]))
+      mid <- mid + K(X[j], X0, h) * Zj %*% t(Zj)
+    }
+    w_val <- (t(z) %*% solve(mid) %*% (K(Xi, X0, h) * Zi)) %>% as.numeric()
+    
+  }else if (type == "poly"){
+    p <- h
+    Zi <- z(X0, p)
+    zx <- z(Xi, p)
+    
+    mid <- matrix(0, p+1, p+1)
+    
+    for(j in 1:length(X)){
+      Zj <- z(X[j], p)
+      mid <- mid +  Zj %*% t(Zj)
+    }
+    
+    w_val <-t(zx) %*% solve(mid) %*% Zi %>% as.numeric()
+  
+  }else stop("not implemented")
+  
+  return(w_val)
+}
 
+pvals <- seq(1,10)
+CV_results_poly <- data.frame(h = pvals, CV = 0)
 
+for(p in pvals){
+  print(p)
+  try(
+    CV_results_poly$CV[p] <- CV(p, X, Y, type = "poly")
+  )
+}
 
+write_csv(CV_results_poly, paste0(out, "poly_CV.csv"))
+ggplot(CV_results_poly) + 
+  geom_line(aes(x = h, y = CV)) + 
+  ggsave(paste0(out, "/poly_CV.png"))
+
+# All the polys on one plot
+get_df_poly <- function(h) {
+  map_dfr(range, m, X = X, h = h, Y = Y, type = "poly") %>% 
+    mutate(h = h)
+}
+plot_df_poly <- map_dfr(seq(1,4), get_df_poly)
+
+ggplot(plot_df_poly) + 
+  geom_line(aes(x = x, y = m, color = factor(h), group =h)) + 
+  geom_point(data = df, aes(x = log_inc, y = share_food), alpha = 0.2) 
+
+plot_df_poly %>% filter(h == 2) %>% 
+  ggplot() + 
+    geom_line(aes(x = x, y = m)) + 
+    ggtitle(paste0("Polynomial Series Regression, p = ", 2)) + 
+    geom_point(data = df, aes(x = log_inc, y = share_food), alpha = 0.2) +
+    ylim(c(0,1)) + xlim(5.5, 9) + 
+    ggsave(paste0(out, "poly2_w_scatter.png"))
 
