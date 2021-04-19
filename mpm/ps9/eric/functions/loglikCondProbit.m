04dce502a4863be7436f0a8d96a296797f22b7d9
Eric Qian <eyqian@gmail.com>
2021-04-19 09:54:57 -0400

    add comments

--- mpm/ps9/eric/functions/loglikCondProbit.m
+++ mpm/ps9/eric/functions/loglikCondProbit.m
@@ -10,14 +10,21 @@ theta = theta(:);
 % for i = 1:n 
 %    X_i    = squeeze(X(i, :, :)); 
 %    Y_i    = Y(i, :);
-%    loglik = loglik + theta'*X_i*Y_i' ...
-%     - log(sum(exp(theta'*X_i)));
+%    loglik = loglik + ...
+%             theta'*X_i*Y_i' ...   % First term
+%     - log(sum(exp(theta'*X_i)));  % Second term
 % end
 
-% Vectorized version
+% Vectorized version. See above code reference
 loglik =...
-    theta'*sum(squeeze(sum(X .* permute(repmat(Y, 1, 1, k), [1 3 2]), 3)))' ...  
-    -sum(log(sum(exp(squeeze(sum(permute(repmat(theta, 1, nOpts, n), [3 1 2]) .* X, 2))), 2)));
+    theta'*sum(squeeze(... % First term
+    sum(X .* permute(repmat(Y, 1, 1, k), [1 3 2]), 3)))' ...  
+     -sum(...  % Second term: sum across n observations
+    log(...
+    sum(...  
+    exp(...
+    squeeze(sum(... theta'*X_i
+    permute(repmat(theta, 1, nOpts, n), [3 1 2]) .* X, 2))), 2)));  % Reorganize
 
 
 
